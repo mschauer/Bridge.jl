18182672303aeb7ba17acf9fadd6a248caab3672
autodocs <autodocs>
2017-08-14 09:35:49 +0000

    build based on fc46725

--- latest/search_index.js
+++ latest/search_index.js
@@ -49,11 +49,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#Bridge.ODESolver",
+    "page": "Bridge.jl",
+    "title": "Bridge.ODESolver",
+    "category": "Type",
+    "text": "ODESolver\n\nAbstract (super-)type for solving methods for ordinary differential equations.\n\n\n\n"
+},
+
+{
     "location": "index.html#Bridge.solve!",
     "page": "Bridge.jl",
     "title": "Bridge.solve!",
     "category": "Function",
-    "text": "solve!(method, X::SamplePath, x0, P) -> X, [err]\n\nSolve ordinary differential equation (ddx) x(t) = F(t x(t) P) on the fixed grid X.tt writing into X.yy \n\nmethod::R3 - using a non-adaptive Ralston (1965) update (order 3).\n\nmethod::BS3 use non-adaptive Bogacki–Shampine method to give error estimate.\n\nCall _solve! to inline. \"Pretty fast if x is a bitstype or a StaticArray.\"\n\n\n\n"
+    "text": "solve!(method, F, X::SamplePath, x0, P) -> X, [err]\n\nSolve ordinary differential equation (ddx) x(t) = F(t x(t) P) on the fixed grid X.tt writing into X.yy \n\nmethod::R3 - using a non-adaptive Ralston (1965) update (order 3).\n\nmethod::BS3 use non-adaptive Bogacki–Shampine method to give error estimate.\n\nCall _solve! to inline. \"Pretty fast if x is a bitstype or a StaticArray.\"\n\n\n\n"
 },
 
 {
@@ -61,7 +69,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Bridge.jl",
     "title": "Bridge.R3",
     "category": "Type",
-    "text": "R3\n\nRalston (1965) update (order 3 step of the Bogacki–Shampine 1989 method) to solve y(t + dt) - y(t) = int_t^t+dt f(s y(s)) ds.\n\n\n\n"
+    "text": "R3\n\nRalston (1965) update (order 3 step of the Bogacki–Shampine 1989 method) to solve y(t + dt) - y(t) = int_t^t+dt F(s y(s)) ds.\n\n\n\n"
 },
 
 {
@@ -69,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Bridge.jl",
     "title": "Bridge.BS3",
     "category": "Type",
-    "text": "BS3\n\nRalston (1965) update (order 3 step of the Bogacki–Shampine 1989 method) to solve y(t + dt) - y(t) = int_t^t+dt f(s y(s)) ds. Uses Bogacki–Shampine method  to give error estimate. \n\n\n\n"
+    "text": "BS3\n\nRalston (1965) update (order 3 step of the Bogacki–Shampine 1989 method) to solve y(t + dt) - y(t) = int_t^t+dt F(s y(s)) ds. Uses Bogacki–Shampine method  to give error estimate. \n\n\n\n"
 },
 
 {
@@ -77,7 +85,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Bridge.jl",
     "title": "Ordinary differential equations",
     "category": "section",
-    "text": "solve!\nBridge.R3\nBridge.BS3"
+    "text": "Bridge.ODESolver\nsolve!\nBridge.R3\nBridge.BS3"
 },
 
 {
@@ -153,11 +161,59 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#Bridge.euler",
+    "page": "Bridge.jl",
+    "title": "Bridge.euler",
+    "category": "Function",
+    "text": "euler(u, W, P) -> X\n\nSolve stochastic differential equation dX_t = b(tX_t)dt + (tX_t)dW_t using the Euler scheme.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.euler!",
+    "page": "Bridge.jl",
+    "title": "Bridge.euler!",
+    "category": "Function",
+    "text": "euler!(Y, u, W, P) -> X\n\nSolve stochastic differential equation dX_t = b(tX_t)dt + (tX_t)dW_t  using the Euler scheme in place.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.thetamethod",
+    "page": "Bridge.jl",
+    "title": "Bridge.thetamethod",
+    "category": "Function",
+    "text": "thetamethod(u, W, P, theta=0.5)\n\nSolve stochastic differential equation using the theta method and Newton-Raphson steps.\n\n\n\n"
+},
+
+{
     "location": "index.html#Stochastic-differential-equations-1",
     "page": "Bridge.jl",
     "title": "Stochastic differential equations",
     "category": "section",
-    "text": "sample\nsample!\nquvar\nbracket\nito\ngirsanov\nlp\nllikelihood"
+    "text": "sample\nsample!\nquvar\nbracket\nito\ngirsanov\nlp\nllikelihood\neuler\neuler!\nthetamethod"
+},
+
+{
+    "location": "index.html#Bridge.GammaProcess",
+    "page": "Bridge.jl",
+    "title": "Bridge.GammaProcess",
+    "category": "Type",
+    "text": "GammaProcess\n\nA GammaProcess with jump rate γ and inverse jump size λ has increments Gamma(t*γ, 1/λ) and Levy measure\n\n(x)= x^-1exp(- x) \n\nHere Gamma(α,θ) is the Gamma distribution in julia's parametrization with shape parameter α and scale θ.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.nu",
+    "page": "Bridge.jl",
+    "title": "Bridge.nu",
+    "category": "Function",
+    "text": " nu(k,P)\n\n(Bin-wise) integral of the Levy measure nu(B_k).\n\n\n\n"
+},
+
+{
+    "location": "index.html#Levy-processes-1",
+    "page": "Bridge.jl",
+    "title": "Levy processes",
+    "category": "section",
+    "text": "GammaProcess\nBridge.nu "
 },
 
 {
@@ -169,27 +225,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Miscellaneous-1",
+    "location": "index.html#Bridge.inner",
     "page": "Bridge.jl",
-    "title": "Miscellaneous",
-    "category": "section",
-    "text": "Bridge.endpoint!"
-},
-
-{
-    "location": "index.html#Bridge.ODESolver",
-    "page": "Bridge.jl",
-    "title": "Bridge.ODESolver",
-    "category": "Type",
-    "text": "ODESolver\n\nAbstract (super-)type for solving methods for ordinary differential equations.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.mcbandmean",
-    "page": "Bridge.jl",
-    "title": "Bridge.mcbandmean",
+    "title": "Bridge.inner",
     "category": "Function",
-    "text": "mcmeanband(mc)\n\nCompute marginal confidence interval for the chain mean using normal approximation\n\n\n\n"
+    "text": "inner(x[, y])\n\nShort-hand for quadratic form x'x (or x'y).\n\n\n\n"
 },
 
 {
@@ -201,99 +241,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Bridge.LocalGammaProcess",
+    "location": "index.html#Bridge.mat",
     "page": "Bridge.jl",
-    "title": "Bridge.LocalGammaProcess",
-    "category": "Type",
-    "text": "LocalGammaProcess\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.Ptilde",
-    "page": "Bridge.jl",
-    "title": "Bridge.Ptilde",
-    "category": "Type",
-    "text": "Ptilde(cs::CSpline, σ)\n\nAffine diffusion dX = cs(t) dt + dW  with cs a \n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.GuidedProp",
-    "page": "Bridge.jl",
-    "title": "Bridge.GuidedProp",
-    "category": "Type",
-    "text": "GuidedProp\n\nGeneral bridge proposal process\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.compensator0",
-    "page": "Bridge.jl",
-    "title": "Bridge.compensator0",
+    "title": "Bridge.mat",
     "category": "Function",
-    "text": "compensator0(kstart, P::LocalGammaProcess)\n\nCompensator of GammaProcess approximating the LocalGammaProcess. For kstart == 1 (only choice) this is nu_0(b_1infty).\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.mcstart",
-    "page": "Bridge.jl",
-    "title": "Bridge.mcstart",
-    "category": "Function",
-    "text": "mcstart(x) -> state\n\nCreate state for random chain online statitics. The entries/value of x are ignored\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.CSpline",
-    "page": "Bridge.jl",
-    "title": "Bridge.CSpline",
-    "category": "Type",
-    "text": "CSpline(s, t, x, y = x, m0 = (y-x)/(t-s), m1 = (y-x)/(t-s))\n\nCubic spline parametrized by f(s) = x and f(t) = y, f(s) = m0, f(t) = m1.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.tofs",
-    "page": "Bridge.jl",
-    "title": "Bridge.tofs",
-    "category": "Function",
-    "text": "tofs(s, T1, T2)\n\nTime change mapping t in [T1, T2] (X-time) to s in [T1, T2] (U-time).\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.r",
-    "page": "Bridge.jl",
-    "title": "Bridge.r",
-    "category": "Function",
-    "text": "r(t, x, T, v, P)\n\nReturns r(tx) = operatornamegrad_x log p(tx T v) where p is the transition density of the process P.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.θ",
-    "page": "Bridge.jl",
-    "title": "Bridge.θ",
-    "category": "Function",
-    "text": "θ(x, P::LocalGammaProcess)\n\nInverse jump size compared to gamma process with same alpha and beta.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.gpK!",
-    "page": "Bridge.jl",
-    "title": "Bridge.gpK!",
-    "category": "Function",
-    "text": "gpK!(K::SamplePath, P)\n\nPrecompute K = H^-1 from (ddt)K = BK + KB + a for a guided proposal.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.mcnext",
-    "page": "Bridge.jl",
-    "title": "Bridge.mcnext",
-    "category": "Function",
-    "text": "mcnext(state, x) -> state\n\nUpdate random chain online statistics when new chain value x was observed. Return new state.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.compensator",
-    "page": "Bridge.jl",
-    "title": "Bridge.compensator",
-    "category": "Function",
-    "text": "compensator(kstart, P::LocalGammaProcess)\n\nCompensator of LocalGammaProcess  For kstart = 1, this is sum_k=1^N nu(B_k), for kstart = 0, this is sum_k=0^N nu(B_k) - C (where C is a constant).\n\n\n\n"
+    "text": "mat(X::SamplePath{SVector}) \nmat(yy::Vector{SVector})\n\nReinterpret X or yy to an array without change in memory.\n\n\n\n"
 },
 
 {
@@ -305,6 +257,14 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#Bridge.CSpline",
+    "page": "Bridge.jl",
+    "title": "Bridge.CSpline",
+    "category": "Type",
+    "text": "CSpline(s, t, x, y = x, m0 = (y-x)/(t-s), m1 = (y-x)/(t-s))\n\nCubic spline parametrized by f(s) = x and f(t) = y, f(s) = m_0, f(t) = m_1.\n\n\n\n"
+},
+
+{
     "location": "index.html#Bridge.integrate",
     "page": "Bridge.jl",
     "title": "Bridge.integrate",
@@ -313,19 +273,91 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Bridge.soft",
+    "location": "index.html#Bridge.logpdfnormal",
     "page": "Bridge.jl",
-    "title": "Bridge.soft",
+    "title": "Bridge.logpdfnormal",
     "category": "Function",
-    "text": "soft(t, T1, T2)\n\nTime change mapping s in [T1, T2](U-time) tot`in[T1, T2](X`-time).\n\n\n\n"
+    "text": "logpdfnormal(x, A)\n\nlogpdf of centered gaussian with covariance A\n\n\n\n"
 },
 
 {
-    "location": "index.html#Bridge.nu",
+    "location": "index.html#Miscellaneous-1",
     "page": "Bridge.jl",
-    "title": "Bridge.nu",
+    "title": "Miscellaneous",
+    "category": "section",
+    "text": "Bridge.endpoint!\nBridge.inner\nBridge.cumsum0\nBridge.mat\nBridge.outer\nCSpline\nBridge.integrate \nBridge.logpdfnormal"
+},
+
+{
+    "location": "index.html#Bridge.mcstart",
+    "page": "Bridge.jl",
+    "title": "Bridge.mcstart",
     "category": "Function",
-    "text": " nu(k,P)\n\n(Bin-wise) integral of the Levy measure nu(B_k).\n\n\n\n"
+    "text": "mcstart(x) -> state\n\nCreate state for random chain online statitics. The entries/value of x are ignored\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.mcnext",
+    "page": "Bridge.jl",
+    "title": "Bridge.mcnext",
+    "category": "Function",
+    "text": "mcnext(state, x) -> state\n\nUpdate random chain online statistics when new chain value x was observed. Return new state.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.mcband",
+    "page": "Bridge.jl",
+    "title": "Bridge.mcband",
+    "category": "Function",
+    "text": "mcband(mc)\n\nCompute marginal 95% coverage interval for the chain from normal approximation.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.mcbandmean",
+    "page": "Bridge.jl",
+    "title": "Bridge.mcbandmean",
+    "category": "Function",
+    "text": "mcmeanband(mc)\n\nCompute marginal confidence interval for the chain mean using normal approximation\n\n\n\n"
+},
+
+{
+    "location": "index.html#Online-statistics-1",
+    "page": "Bridge.jl",
+    "title": "Online statistics",
+    "category": "section",
+    "text": "Online updating of the tuple state = (m, m2, n) wherem - mean(x[1:n])m2 - sum of squares of differences from the current mean, textstylesum_i=1^n (x_i - bar x_n)^2n - number of iterationsmcstart\nmcnext\nmcband\nmcbandmean"
+},
+
+{
+    "location": "index.html#Bridge.LinPro",
+    "page": "Bridge.jl",
+    "title": "Bridge.LinPro",
+    "category": "Type",
+    "text": "LinPro(B, μ::T, σ)\n\nLinear diffusion dX = B(X - )dt + dW.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.Ptilde",
+    "page": "Bridge.jl",
+    "title": "Bridge.Ptilde",
+    "category": "Type",
+    "text": "Ptilde(cs::CSpline, σ)\n\nAffine diffusion dX = cs(t) dt + dW  with cs a cubic spline ::CSpline.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Linear-Processes-1",
+    "page": "Bridge.jl",
+    "title": "Linear Processes",
+    "category": "section",
+    "text": "LinPro\nBridge.Ptilde"
+},
+
+{
+    "location": "index.html#Bridge.GuidedProp",
+    "page": "Bridge.jl",
+    "title": "Bridge.GuidedProp",
+    "category": "Type",
+    "text": "GuidedProp\n\nGeneral bridge proposal process\n\n\n\n"
 },
 
 {
@@ -345,11 +377,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Bridge.logpdfnormal",
+    "location": "index.html#Bridge.mdb",
     "page": "Bridge.jl",
-    "title": "Bridge.logpdfnormal",
+    "title": "Bridge.mdb",
     "category": "Function",
-    "text": "logpdfnormal(x, A)\n\nlogpdf of centered gaussian with covariance A\n\n\n\n"
+    "text": "mdb(u, W, P)\nmdb!(copy(W), u, W, P)\n\nEuler scheme with the diffusion coefficient correction of the modified diffusion bridge.\n\n\n\n"
 },
 
 {
@@ -361,19 +393,75 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Bridge.euler!",
+    "location": "index.html#Bridge.r",
     "page": "Bridge.jl",
-    "title": "Bridge.euler!",
+    "title": "Bridge.r",
     "category": "Function",
-    "text": "euler!(Y, u, W, P) -> X\n\nSolve stochastic differential equation dX_t = b(tX_t)dt + (tX_t)dW_t  using the Euler scheme in place.\n\n\n\n"
+    "text": "r(t, x, T, v, P)\n\nReturns r(tx) = operatornamegrad_x log p(tx T v) where p is the transition density of the process P.\n\n\n\n"
 },
 
 {
-    "location": "index.html#Bridge.GammaProcess",
+    "location": "index.html#Bridge.gpK!",
     "page": "Bridge.jl",
-    "title": "Bridge.GammaProcess",
+    "title": "Bridge.gpK!",
+    "category": "Function",
+    "text": "gpK!(K::SamplePath, P)\n\nPrecompute K = H^-1 from (ddt)K = BK + KB + a for a guided proposal.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridges-1",
+    "page": "Bridge.jl",
+    "title": "Bridges",
+    "category": "section",
+    "text": "GuidedProp\nbridge\nBridge.Vs\nBridge.mdb \nBridge.mdb!\nBridge.r\nBridge.gpK! "
+},
+
+{
+    "location": "index.html#Bridge.LocalGammaProcess",
+    "page": "Bridge.jl",
+    "title": "Bridge.LocalGammaProcess",
     "category": "Type",
-    "text": "GammaProcess\n\nA GammaProcess with jump rate γ and inverse jump size λ has increments Gamma(t*γ, 1/λ) and Levy measure\n\n(x)= x^-1exp(- x) \n\nHere Gamma(α,θ) is the Gamma distribution in julia's parametrization with shape parameter α and scale θ.\n\n\n\n"
+    "text": "LocalGammaProcess\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.compensator0",
+    "page": "Bridge.jl",
+    "title": "Bridge.compensator0",
+    "category": "Function",
+    "text": "compensator0(kstart, P::LocalGammaProcess)\n\nCompensator of GammaProcess approximating the LocalGammaProcess. For kstart == 1 (only choice) this is nu_0(b_1infty).\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.compensator",
+    "page": "Bridge.jl",
+    "title": "Bridge.compensator",
+    "category": "Function",
+    "text": "compensator(kstart, P::LocalGammaProcess)\n\nCompensator of LocalGammaProcess  For kstart = 1, this is sum_k=1^N nu(B_k), for kstart = 0, this is sum_k=0^N nu(B_k) - C (where C is a constant).\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.θ",
+    "page": "Bridge.jl",
+    "title": "Bridge.θ",
+    "category": "Function",
+    "text": "θ(x, P::LocalGammaProcess)\n\nInverse jump size compared to gamma process with same alpha and beta.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.soft",
+    "page": "Bridge.jl",
+    "title": "Bridge.soft",
+    "category": "Function",
+    "text": "soft(t, T1, T2)\n\nTime change mapping s in [T1, T2](U-time) tot`in[T1, T2](X`-time).\n\n\n\n"
+},
+
+{
+    "location": "index.html#Bridge.tofs",
+    "page": "Bridge.jl",
+    "title": "Bridge.tofs",
+    "category": "Function",
+    "text": "tofs(s, T1, T2)\n\nTime change mapping t in [T1, T2] (X-time) to s in [T1, T2] (U-time).\n\n\n\n"
 },
 
 {
@@ -385,59 +473,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Bridge.mdb",
-    "page": "Bridge.jl",
-    "title": "Bridge.mdb",
-    "category": "Function",
-    "text": "mdb(u, W, P)\nmdb!(copy(W), u, W, P)\n\nEuler scheme with the diffusion coefficient correction of the modified diffusion bridge.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.euler",
-    "page": "Bridge.jl",
-    "title": "Bridge.euler",
-    "category": "Function",
-    "text": "euler(u, W, P) -> X\n\nSolve stochastic differential equation dX_t = b(tX_t)dt + (tX_t)dW_t using the Euler scheme.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.mat",
-    "page": "Bridge.jl",
-    "title": "Bridge.mat",
-    "category": "Function",
-    "text": "mat(X::SamplePath{SVector}) \nmat(yy::Vector{SVector})\n\nReinterpret X or yy to an array without change in memory.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.thetamethod",
-    "page": "Bridge.jl",
-    "title": "Bridge.thetamethod",
-    "category": "Function",
-    "text": "thetamethod(u, W, P, theta=0.5)\n\nSolve stochastic differential equation using the theta method and Newton-Raphson steps.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.mcband",
-    "page": "Bridge.jl",
-    "title": "Bridge.mcband",
-    "category": "Function",
-    "text": "mcband(mc)\n\nCompute marginal 95% coverage interval for the chain from normal approximation.\n\n\n\n"
-},
-
-{
-    "location": "index.html#Bridge.LinPro",
-    "page": "Bridge.jl",
-    "title": "Bridge.LinPro",
-    "category": "Type",
-    "text": "LinPro(B, μ::T, σ)\n\nLinear diffusion dX = B(X - )dt + dW\n\n\n\n"
-},
-
-{
     "location": "index.html#Unsorted-1",
     "page": "Bridge.jl",
     "title": "Unsorted",
     "category": "section",
-    "text": "Bridge.ODESolver\nmcbandmean \nBridge.cumsum0 \nLocalGammaProcess\nBridge.Ptilde\nGuidedProp\nBridge.compensator0 \nmcstart \nCSpline \nBridge.tofs\nBridge.r\nBridge.θ \nBridge.gpK! \nmcnext \nBridge.compensator \nBridge.outer \nBridge.integrate \nBridge.soft\nBridge.nu \nbridge\nBridge.Vs\nBridge.logpdfnormal\nBridge.mdb!\neuler! \nGammaProcess\nBridge.dotVs\nBridge.mdb \neuler \nBridge.mat \nthetamethod \nmcband \nLinPro"
+    "text": "LocalGammaProcess\nBridge.compensator0 \nBridge.compensator\nBridge.θ \nBridge.soft\nBridge.tofs\nBridge.dotVs"
 },
 
 ]}
