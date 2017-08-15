f76c0c3fc7a6101abf606126903901872072ba5a
mschauer <moritzschauer@web.de>
2017-08-15 14:21:39 +0200

    Test for SDE schemes

--- test/euler.jl
+++ test/euler.jl
@@ -1,18 +1,68 @@
-using Bridge
+using Bridge, StaticArrays
 using Base.Test
 
-brown1(s, t, n) = sample(linspace(s, t, n), Wiener{Float64}())
- 
 
-function f(n)
-    W = brown1(0.0,1.0,1000)
-    X = copy(W)
-    s = 0.
-    for i in 1:n 
-        X.yy .= 0
-        s += Bridge.euler!(X, 0.0,W, WienerBridge(2.0, 1.0)).yy[end]
-    end
-    s
+n = 5000
+T = (0.0, 10.0)
+dt = (T[end]-T[1])/n
+tt = collect(linspace(T[1], T[2], n+1))
+vx = [1.0,0.0,0.0]
+
+
+const SV = SVector{3, Float64}
+
+
+
+
+type VLorenz <: ContinuousTimeProcess{Vector{Float64}}
 end
 
-@time f(1000)
+Bridge.b(t, u, ::VLorenz) = [10.0(u[2]-u[1]), u[1]*(28.0-u[3]) - u[2], u[1]*u[2] - (8/3)*u[3]]
+function Bridge.b!(t, u, du, ::VLorenz)
+    @inbounds du[1] = 10.0(u[2]-u[1])
+    @inbounds du[2] = u[1]*(28.0-u[3]) - u[2]
+    @inbounds du[3] = u[1]*u[2] - (8/3)*u[3]
+end
+Bridge.σ(t, u, ::VLorenz) = 3.0I
+function Bridge.σ!(t, u, dw, dm, ::VLorenz) 
+    @inbounds dm[1] = 3*dw[1]
+    @inbounds dm[2] = 3*dw[2]
+    @inbounds dm[3] = 3*dw[3]
+end
+
+
+
+W = sample(tt, Wiener{SV}())
+VW = VSamplePath(tt, Bridge.mat(W.yy))
+VX = VSamplePath(tt, copy(VW.yy))
+VX2 = VSamplePath(tt, copy(VW.yy))
+Bridge.solve!(Bridge.EulerMaruyama!(), VX, vx, VW, VLorenz())
+Bridge.solve!(Bridge.EulerMaruyama(), VX2, vx, VW, VLorenz())
+
+
+######
+
+type Lorenz <: ContinuousTimeProcess{SV}
+end
+
+Bridge.b(s, u, ::Lorenz) = @SVector [10.0(u[2]-u[1]), u[1]*(28.0-u[3]) - u[2], u[1]*u[2] - (8/3)*u[3]           ]
+Bridge.σ(t, u, ::Lorenz) = 3.0I
+
+
+u = SVector{3}(vx)
+X = Bridge.euler(u, W, Lorenz())
+X2 = copy(X)
+X3 = copy(X)
+
+Bridge.solve!(Euler(), X, u, W, Lorenz())
+Bridge.euler!(X2, u, W, Lorenz())
+invoke(Bridge.solve!, Tuple{Bridge.EulerMaruyama,Any,SVector{3,Float64},Bridge.AbstractPath,Lorenz},
+Bridge.EulerMaruyama(), X3, u, W, Lorenz())
+
+@test maximum(abs.(VW.yy - Bridge.mat(W.yy))) < eps()
+
+@test maximum(abs.(VX.yy - Bridge.mat(X.yy))) < eps()
+@test maximum(abs.(VX.yy - VX2.yy)) < eps()
+@test maximum(norm.(X.yy - X2.yy)) < eps()
+@test maximum(norm.(X.yy - X3.yy)) < eps()
+
