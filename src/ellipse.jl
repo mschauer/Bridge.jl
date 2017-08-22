c69a6bea9292e354772558294d1a53a265e4fc84
mschauer <moritzschauer@web.de>
2017-08-22 20:44:31 +0200

    ambiguity appears to be resolved

--- src/ellipse.jl
+++ src/ellipse.jl
@@ -1,9 +1,6 @@
 import Base.getindex, Base.setindex!
 const .. = Val{:...}
 
-setindex!(A::AbstractArray{T,Ti}, x, ::Type{Val{:...}}, ::Base.Colon) where {T<:Any, Ti<:Integer} = throw( ArgumentError("cannot combine ellipse with colon this way"))
-getindex(::Base.SparseArrays.SparseMatrixCSC, ::Type{Val{:...}}, ::Base.Colon) = throw( ArgumentError("cannot combine ellipse with colon this way"))
-
 setindex!(A::AbstractArray{T,1}, x, ::Type{Val{:...}}, n) where {T} = A[n] = x
 setindex!(A::AbstractArray{T,2}, x, ::Type{Val{:...}}, n) where {T} = A[ :, n] = x
 setindex!(A::AbstractArray{T,3}, x, ::Type{Val{:...}}, n) where {T} = A[ :, :, n] =x
