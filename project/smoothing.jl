1f6ba4f9305cf439406dcf606ac0deb750a7bb76
mschauer <moritzschauer@web.de>
2017-12-05 14:30:08 +0100

    Tweaks to smoothign.jl

--- project/smoothing.jl
+++ project/smoothing.jl
@@ -4,7 +4,7 @@
 #end
 using JLD
 
-simid = 2
+simid = 1
 sim = [:lorenz1, :lorenz2, :lorenz3][simid]
 simname = String(sim)
 
@@ -15,7 +15,7 @@ end
 
 iterations = 50000
 saveit = 5000
-rho = 0.02 # 1 - rho is AR(1) coefficient of Brownian motion valued random walk  
+alpha = 0.5 # 1 - exp(-alpha Exp) is AR(1) coefficient of Brownian motion valued random walk  
 independent = false # independent proposals
 adaptive = true # adaptive proposals
 adaptit = 1000 # adapt every `it`th step
@@ -24,25 +24,22 @@ adaptmax = iterations
 
 if simid == 4
     partial = true
-    initnu = :backwards
+    initnu = :backward
 elseif simid == 3
     partial = false
     adaptive = true
-    initnu = :backwards
-    #julia> acc/iterations
-    #0.11502
+    initnu = :brown
+    alpha = 0.5
 elseif simid == 2
     partial = false
     adaptive = false
-    initnu = :backwards
-    rho = 0.01
+    initnu = :backward
 elseif simid == 1
     partial = false
     adaptive = false
     initnu = :brown
     #julia> acc/iterations
-    #0.08496
-    rho = 0.01
+    alpha = 5.0
 else 
     error("provide `simid in 1:3`")
 end
@@ -59,14 +56,17 @@ WWᵒ = Vector{typeof(W)}(m)
 
 # Create linear noise approximations
 
+if initnu == :backward
+    TPt = Bridge.LinearNoiseAppr{Bridge.Models.Lorenz,StaticArrays.SDiagonal{3,Float64},SVector{3,Float64}}
+else
+    TPt = Bridge.LinearAppr{StaticArrays.SDiagonal{3,Float64},StaticArrays.SArray{Tuple{3,3},Float64,2,9},SVector{3,Float64}}
+end
+TPᵒ = Bridge.GuidedBridge{SVector{3,Float64},StaticArrays.SArray{Tuple{3,3},Float64,2,9},TPt,Bridge.Models.Lorenz}
 
-TPt = Bridge.LinearNoiseAppr{Bridge.Models.Lorenz,StaticArrays.SDiagonal{3,Float64},SVector{3,Float64}}
-TPᵒ = Bridge.GuidedBridge{SVector{3,Float64},StaticArrays.SArray{Tuple{3,3},Float64,2,9},Bridge.LinearNoiseAppr{Bridge.Models.Lorenz,StaticArrays.SDiagonal{3,Float64},SVector{3,Float64}},Bridge.Models.Lorenz}
-
-
-Pt = Vector{TPt}(m)
-Pᵒ = Vector{TPᵒ}(m)
-
+#Pt = Vector{TPt}(m)
+#Pᵒ = Vector{TPᵒ}(m)
+Pt = Vector(m)
+Pᵒ = Vector(m)
 H♢, v = Bridge.gpupdate(πH*one(SM), zero(SV), L, Σ, V.yy[end])
 
 for i in m:-1:1
@@ -74,17 +74,28 @@ for i in m:-1:1
     WW[i] = SamplePath(W.tt[1 + (i-1)*M:1 + i*M], W.yy[1 + (i-1)*M:1 + i*M])
     
     a_ = Bridge.a(XX[i].tt[end], v, P)
-    Pt[i] = Bridge.LinearNoiseAppr(XX[i].tt, P, v, a_, :backward) 
+
+ #   Pt[i] = Bridge.LinearNoiseAppr(XX[i].tt, P, v, a_, :backward) 
     if initnu == :brown
-        Pt[i].Y.yy[:] *= 0
+        Pt[i] = Bridge.linearappr(SamplePath(XX[i].tt, zeros(ℝ{3}, M+1)), Bridge.NoDrift(P))
+    elseif initnu == :backward
+        Pt[i] = Bridge.LinearNoiseAppr(XX[i].tt, P, v, a_, :backward) 
+    elseif initnu == :foci
+        if partial
+            x_ = Models.foci(P)[1 + ( V.yy[i][1] > 0)]
+        else
+            x_ = Models.foci(P)[1 + ( V.yy[i][2] + V.yy[i][3] > 0)]
+        end
+        Pt[i] = Bridge.linearappr(SamplePath(XX[i].tt, fill(x_, M+1)), P)
     end
+  
 
     Pᵒ[i] = Bridge.GuidedBridge(XX[i].tt, P, Pt[i], v, H♢)
     H♢, v = Bridge.gpupdate(Pᵒ[i], L, Σ, V.yy[i])
 end
  
 
-π0 = Bridge.Gaussian(v, H♢)
+π0 = Bridge.Gaussian(v, Hermitian(H♢))
 
 y = π0.μ
 for i in 1:m
@@ -95,15 +106,16 @@ XXmean = [zero(XX[i]) for i in 1:m]
 
 #X0 = ℝ{3}[]
 
-function smooth(π0, XX, WW, P, Pᵒ, iterations, rho; verbose = true,adaptive = true, adaptmax = iterations, adaptit = 5000, saveit = 500, independent = false, hwindow=20)
+function smooth(π0, XX, WW, P, Pᵒ, iterations, alpha; verbose = true,adaptive = true, adaptmax = iterations, adaptit = 5000, saveit = 500, independent = false, 
+    smoothmean = false, hwindow=20)
     Paths = []
     
     m = length(XX)
-    rho0 = rho / 2 
+    #rho0 = 1 / alpha 
     # create workspace
     XXᵒ = deepcopy(XX)
     WWᵒ = deepcopy(WW)
-    W = Wiener{valtype(WW[1])}()
+    Wnr = Wiener{valtype(WW[1])}()
 
     # initialize
     mcstate = [mcstart(XX[i].yy) for i in 1:m]
@@ -112,36 +124,61 @@ function smooth(π0, XX, WW, P, Pᵒ, iterations, rho; verbose = true,adaptive =
     X0 = typeof(y0)[]
     Xt = typeof(y0)[]
     for it in 1:iterations
+        doaccept = false
+        newblock = false
         if it % saveit == 0
             push!(Paths, collect(Iterators.flatten(XX[i].yy[1:end-1] for i in 1:m)))
         end
 
         if adaptive && it < adaptmax && it % adaptit == 0 # adaptive smoothing
-            error()
             H♢, v = Bridge.gpupdate(πH*one(SM), zero(SV), L, Σ, V.yy[end])            
             for i in m:-1:1
                 xx = mcstate[i][1]
-                Pt[i].Y.yy[:] += [mean(xx[max(1, j-hwindow):min(end, j+hwindow)]) for j in 1:length(xx)]
-                Pt[i].Y.yy[:] /= 2
-                Pᵒ[i] = Bridge.GuidedBridge(XX[i].tt, P, Pt[i], v, H♢)
+                if isa(Pᵒ[i].Pt, Bridge.LinearNoiseAppr)
+                    if smoothmean
+                        Pᵒ[i].Pt.Y.yy[:] = [mean(xx[max(1, j-hwindow):min(end, j+hwindow)]) for j in 1:length(xx)]
+                    else
+                        Pᵒ[i].Pt.Y.yy[:] = xx
+                    end
+#                Pt[i].Y.yy[:] /= 2
+                elseif isa(Pᵒ[i].Pt, Bridge.LinearAppr)
+                    if smoothmean
+                        Y = SamplePath(XX[i].tt, [mean(xx[max(1, j-hwindow):min(end, j+hwindow)]) for j in 1:length(xx)])
+                    else
+                        Y = SamplePath(XX[i].tt, xx)
+                    end
+                    Bridge.linearappr!(Pᵒ[i].Pt, Y, P)
+                else 
+                    error("Adaption not implemented for proposals Pᵒ.Pt")
+                end
+                Pᵒ[i] = Bridge.GuidedBridge(XX[i].tt, P, Pᵒ[i].Pt, v, H♢)
                 H♢, v = Bridge.gpupdate(Pᵒ[i], L, Σ, V.yy[i])
             end
-            π0 = Bridge.Gaussian(v, H♢)
+            π0 = Bridge.Gaussian(v, Hermitian(H♢))
+            #y0ᵒ = rand(π0) 
+            newblock = true
+            if it == adaptit # first adaptive proposal needs some help sometimes
+                doaccept = true
+            end
+
         end
 
         push!(X0, y0)
         push!(Xt, XX[div(end,2)].yy[1])
-
-        if !independent
-            y0ᵒ = π0.μ + sqrt(rho0)*(rand(π0) - π0.μ) + sqrt(1-rho0)*(y0 - π0.μ) 
+        rho_ = exp(-alpha*randexp())   
+        if newblock
+            #y0ᵒ = rand(π0) 
+            y0ᵒ = y0
+        elseif !independent
+           
+            y0ᵒ = π0.μ + sqrt(rho_)*(rand(π0) - π0.μ) + sqrt(1-rho_)*(y0 - π0.μ) 
         else
             y0ᵒ = rand(π0) 
         end
         y = y0ᵒ
         for i in 1:m
-            sample!(WWᵒ[i], W)
+            sample!(WWᵒ[i], Wnr)
             if !independent
-                rho_ = rho * rand()
                 WWᵒ[i].yy[:] = sqrt(rho_)*WWᵒ[i].yy + sqrt(1-rho_)*WW[i].yy
             end
             y = Bridge.bridge!(XXᵒ[i], y, WWᵒ[i], Pᵒ[i])
@@ -152,21 +189,22 @@ function smooth(π0, XX, WW, P, Pᵒ, iterations, rho; verbose = true,adaptive =
         for i in 1:m
             ll += llikelihood(LeftRule(), XXᵒ[i],  Pᵒ[i]) - llikelihood(LeftRule(), XX[i],  Pᵒ[i])
         end
-        print("$it ll $(round(ll,2)) ")
+        verbose && print(@sprintf("%7i", it), " ll ",  @sprintf("%10.3f", ll))
 
         #if true
-        if  rand() < exp(ll) 
+        if doaccept || rand() < exp(ll) 
             acc += 1
-            verbose && print("\t X")
+            verbose && print("\t ✓")
             y0 = y0ᵒ
             for i in 1:m
                 XX[i], XXᵒ[i] = XXᵒ[i], XX[i]
                 WW[i], WWᵒ[i] = WWᵒ[i], WW[i]
             end
+            newblock = false
         else 
             verbose && print("\t .")
         end
-        verbose && println("\t\t", round.(y0 - x0, 2))
+        verbose && println([" ", "N"][1+newblock], "\t", round(acc/it,2), "\t", round.(y0 - x0, 2))
         for i in 1:m
             mcstate[i] = Bridge.mcnext!(mcstate[i],XX[i].yy)
         end
@@ -174,12 +212,18 @@ function smooth(π0, XX, WW, P, Pᵒ, iterations, rho; verbose = true,adaptive =
     Paths, X0, Xt, mcstate, acc
 end
 
-Paths, X0, Xt, mcstates, acc = smooth(π0, XX, WW, P, Pᵒ, iterations, rho;
+Paths, X0, Xt, mcstates, acc = smooth(π0, XX, WW, P, Pᵒ, iterations, alpha;
 adaptive = adaptive,
 adaptit = adaptit,
 adaptmax = adaptmax,
 saveit = saveit, 
 verbose = true, independent = independent)
+
+open(joinpath("output", simname,"info.txt"), "w") do f
+    println(f, "acc") 
+    println(f, round(acc/iterations, 3)) 
+end
+
 
 writecsv(joinpath("output", simname, "x0n$simid.csv"), [1:iterations Bridge.mat(X0)'])
 writecsv(joinpath("output", simname, "xtn$simid.csv"), [1:iterations Bridge.mat(Xt)'])
@@ -190,6 +234,9 @@ JLD.save(joinpath("output", simname, "results.jld"), "Path", Paths, "mcstates", 
 
 # Plot result
 include("../extra/makie.jl")
+
+Xmeanm, Xstdm = Bridge.mcmarginalstats(mcstates)
+
 
 Xmean, Xrot, Xscal = mcsvd3(mcstates)
 
