01569b0e15499c636613cb7947f34f88f86c18fa
mschauer <moritzschauer@web.de>
2017-12-10 23:40:59 +0100

    Use ribbon instead of fill

--- src/recipes.jl
+++ src/recipes.jl
@@ -8,11 +8,9 @@ end
     X.tt, X.yy'
 end
 
-@recipe function plot(X::EstSamplePath{Float64}; quantile = 0.95)
-    q = quantile(Normal(), 1 - (1 - quantile)/2)
-    u = X.yy + q*sqrt.(X.vv)
-    l = X.yy - q*sqrt.(X.vv)
-    fillrange --> l
-    fillalpha --> 0.5 
-    X.tt, u
+@recipe function plot(X::Bridge.EstSamplePath{Float64}; quantile = 0.95)
+    q = Distributions.quantile(Normal(), 1 - (1 - quantile)/2)
+    ribbon --> q*sqrt.(X.vv)
+    X.tt, X.yy
 end
+
