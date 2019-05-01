8b2569a550f85a693871ae9823e9ae6d98764dc2
mschauer <moritzschauer@web.de>
2019-05-01 10:16:27 +0200

    Test Marcin's recursions

--- src/partialbridgenuH.jl
+++ src/partialbridgenuH.jl
@@ -1,42 +1,112 @@
-function partialbridgeodeνH!(::R3, t, L, Σ, v, νt, Ht, P, ϵ)
+function updateνH⁺(L, Σ, v, ϵ)
     m, d = size(L)
+    @assert m == length(v)
+    H = (L' * inv(Σ) * L + ϵ * I)
+    H⁺ = inv(H)
+    ν = H⁺ * L' * inv(Σ) * v
+    ν, H⁺
+end
+function updateC(L, Σ, v, C = 0.0)
+    m, d = size(L)
+    C += 0.5 * dot(v, Σ\v)
+    C += length(m)/2*log(2pi) + 0.5*logdet(Σ)
+    C
+end
+function updateνH⁺C(L, Σ, v, ϵ)
+    updateνH⁺(L, Σ, v, ϵ)..., updateC(L, Σ, v)
+end
+
+
+function partialbridgeodeνH!(::R3, t, νt, Ht, P, (ν, H⁺, C))
+    #m, d = size(L)
+    #@assert m == length(v)
     # print(typeof(H⁺t))
     # print(typeof(νt))
     # print(typeof(inv(L' * inv(Σ) * L + ϵ * I)   ))
-    Ht[end] = (L' * inv(Σ) * L + ϵ * I)
-    H⁺ = inv(Ht[end])
-    νt[end] = H⁺ * L' * inv(Σ) * v
-    ν = νt[end]
-    F(t, y, P) = B(t, P)*y + β(t,P)
-    Ri(t, y, P) = B(t, P)*y + y * B(t,P)' - a(t, P)
+    #Ht[end] = H = (L' * inv(Σ) * L + ϵ * I)
+    #H⁺ = inv(H)
+    #νt[end] = H⁺ * L' * inv(Σ) * v
+    #F = L' * inv(Σ) * v
+    #ν = νt[end]
+    #C += 0.5 * v' * inv(Σ) * v # 0.5 c
+    #C += m/2*log(2pi) + 0.5*log(abs(det(Σ))) # Q, use logabsdet, actually
+    #ν, H⁺ = zero(νt[end]), zero(Ht[end])
+    #H⁺, ν = gpupdate(H⁺, ν, L, Σ, v)
+    H = inv(H⁺)
+    #C = updateC(L, Σ, v, C)
+
+    b̃(t, y, P) = B(t, P)*y + β(t, P)
+    dH⁺(t, y, P) = B(t, P)*y + y * B(t,P)' - a(t, P)
+#    dH(t, y, P) = - B(t, P)'*y - y * B(t,P) + y*a(t, P)*y'
+#    dF(t, y, (H,P)) = -B(t, P)'*y + H*a(t, P)*y  + H*β(t, P)
+
     for i in length(t)-1:-1:1
         dt = t[i] - t[i+1]
-        ν = kernelr3(F, t[i+1], ν, dt, P)
-        H⁺ = kernelr3(Ri, t[i+1], H⁺, dt, P)
+        ν = kernelr3(b̃, t[i+1], ν, dt, P)
+        H⁺ = kernelr3(dH⁺, t[i+1], H⁺, dt, P)
+        #H = kernelr3(dH, t[i+1], H, dt, P)
+
+
+
+        #F = kernelr3(dF, t[i+1], F, dt, (H,P))
+        F = Ht[i+1]*νt[i+1]
+        #@show F , Ht[i+1]*νt[i+1]
+        C += β(t[i+1], P)'*F*dt  + 0.5*F'*a(t[i+1], P)*F*dt - 0.5*tr(H*a(t[i+1], P))*dt
 
         νt[i] = ν
-        Ht[i] = inv(H⁺)
+        #@show H, inv(H⁺)
+        Ht[i] = H = inv(H⁺)
     end
-    νt, Ht
- end
+
+    νt, Ht, C
+end
+
+function updateFHC(L, Σ, v, F, H, ϵ = 0.0, C = 0.0)
+    m, d = size(L)
+    H += (L' * inv(Σ) * L + ϵ * I)
+    F += L' * inv(Σ) * v
+    F, H, updateC(L, Σ, v, C)
+end
+
+function partialbridgeodeHνH!(::R3, t, Ft, Ht, P, (F, H, C))
+    Ht[end] = H
+    Ft[end] = F
+
+    dH(t, y, P) = - B(t, P)'*y - y * B(t,P) + y*a(t, P)*y'
+    dF(t, y, (H,P)) = -B(t, P)'*y + H*a(t, P)*y  + H*β(t, P)
+
+    for i in length(t)-1:-1:1
+        dt = t[i] - t[i+1]
+        C += β(t[i+1], P)'*F*dt + 0.5*F'*a(t[i+1], P)*F*dt - 0.5*tr(H*a(t[i+1], P))*dt
+        H = kernelr3(dH, t[i+1], H, dt, P)
+        F = kernelr3(dF, t[i+1], F, dt, (H, P))
+        Ft[i] = F
+        Ht[i] = H
+    end
+
+    Ft, Ht, C
+end
+
 
 struct Lyap
 end
-function partialbridgeodeνH!(::Lyap, t, νt, Ht, P, νend, Hend⁺)
-    Ht[end] = inv(Hend⁺)
+function partialbridgeodeνH!(::Lyap, t, νt, Ht, P, (νend, Hend⁺, C))
+    Ht[end] = H = inv(Hend⁺)
     νt[end] = νend
     H⁺ = Hend⁺
     ν = νend
-    F(t, y, P) = B(t, P)*y + β(t,P)
+    b̃(t, y, P) = B(t, P)*y + β(t,P)
 
     for i in length(t)-1:-1:1
         dt = t[i] - t[i+1]
-        ν = kernelr3(F, t[i+1], ν, dt, P)
+        ν = kernelr3(b̃, t[i+1], ν, dt, P)
         H⁺ = lyapunovpsdbackward_step(t[i+1], H⁺, -dt, P)
+        F = Ht[i+1]*νt[i+1]
+        C += β(t[i+1], P)'*F*dt  + 0.5*F'*a(t[i+1], P)*F*dt - 0.5*tr(H*a(t[i+1], P))*dt
         νt[i] = ν
-        Ht[i] = inv(H⁺)
+        Ht[i] = H = inv(H⁺)
     end
-    ν, H⁺
+    ν, H⁺, C
 end
 
 
@@ -56,14 +126,15 @@ Guided proposal process for diffusion bridge using backward recursion.
     linear process `Pt` with backwards equation initialized at `ν, Hend⁺`.
 
 """
-struct PartialBridgeνH{T,TP,TPt,Tν,TH} <: ContinuousTimeProcess{T}
+struct PartialBridgeνH{T,TP,TPt,Tν,TH,TC} <: ContinuousTimeProcess{T}
     Target::TP
     Pt::TPt
     tt::Vector{Float64}
     ν::Vector{Tν}
     H::Vector{TH}
-    PartialBridgeνH(P::TP, Pt::TPt, tt, νt::Vector{Tν}, Ht::Vector{TH}) where {TP,TPt,Tν,TH} =
-        new{Bridge.valtype(P),TP,TPt,Tν,TH}(P, Pt, tt, νt, Ht)
+    C::TC
+    PartialBridgeνH(P::TP, Pt::TPt, tt, νt::Vector{Tν}, Ht::Vector{TH}, C::TC=0.0) where {TP,TPt,Tν,TH,TC} =
+        new{Bridge.valtype(P),TP,TPt,Tν,TH,TC}(P, Pt, tt, νt, Ht, C)
 
 
     # 6-7 arg
@@ -75,8 +146,9 @@ struct PartialBridgeνH{T,TP,TPt,Tν,TH} <: ContinuousTimeProcess{T}
         Ht = zeros(TH, N)
         Tν = typeof(@SVector zeros(d))
         νt = zeros(Tν, N)
-        partialbridgeodeνH!(R3(), tt, L, Σ, v, νt, Ht, Pt, ϵ)
-        PartialBridgeνH(P, Pt, tt, νt, Ht)
+        ν, H⁺, C = updateνH⁺C(L, Σ, v, ϵ)
+        _, _, C = partialbridgeodeνH!(R3(), tt, νt, Ht, Pt, (ν, H⁺, C))
+        PartialBridgeνH(P, Pt, tt, νt, Ht, C)
     end
 end
 # 5 arg
@@ -85,8 +157,8 @@ function partialbridgeνH(tt_, P, Pt, νend::Tv, Hend⁺::TH) where {Tv,TH}
     N = length(tt)
     Ht = zeros(TH, N)
     νt = zeros(Tv, N)
-    ν, H⁺ = partialbridgeodeνH!(Lyap(), tt, νt, Ht, Pt, νend, Hend⁺)
-    PartialBridgeνH(P, Pt, tt, νt, Ht), ν, H⁺
+    ν, H⁺, C = partialbridgeodeνH!(Lyap(), tt, νt, Ht, Pt, (νend, Hend⁺, C))
+    PartialBridgeνH(P, Pt, tt, νt, Ht, C), ν, H⁺, C
 end
 
 function _b((i,t)::IndexedTime, x, P::PartialBridgeνH)
@@ -101,7 +173,7 @@ a(t, x, P::PartialBridgeνH) = a(t, x, P.Target)
 Γ(t, x, P::PartialBridgeνH) = Γ(t, x, P.Target)
 constdiff(P::PartialBridgeνH) = constdiff(P.Target) && constdiff(P.Pt)
 
-
+lptilde(x, P::PartialBridgeνH) = -0.5*((P.ν[1] - x)'*P.H[1] * (P.ν - x)) - P.C
 
 function llikelihood(::LeftRule, Xcirc::SamplePath, Po::PartialBridgeνH; skip = 0)
     tt = Xcirc.tt
--- test/partialbridge!.jl
+++ test/partialbridge!.jl
@@ -2,83 +2,35 @@ using Bridge, Distributions
 using Test, Statistics, Random, LinearAlgebra
 using Bridge.Models
 
-T = 2.0
-dt = 1/1000
+include("partialparam.jl")
+x0 = Vector(x0)
+L = Matrix(L)
+Σ = Matrix(Σ)
+v = Vector(v)
 
-tt = 0.:dt:T
-struct PBIntegratedDiffusion <: ContinuousTimeProcess{Float64}
-    γ::Float64
-end
-struct PBIntegratedDiffusionAux <: ContinuousTimeProcess{Float64}
-    γ::Float64
-end
-
-PorPtilde = Union{PBIntegratedDiffusion, PBIntegratedDiffusionAux}
-
-
-βu(t, x::Float64, P::PBIntegratedDiffusion) = - (x+sin(x)) + 1/2
-βu(t, x::Float64, P::PBIntegratedDiffusionAux) = -x + 1/2
-# not really a 'beta'
-
-Bridge.b(t::Float64, x, P::PorPtilde) = Bridge.b!(t, x, copy(x), P)
-function Bridge.b!(t, x, out, P::PorPtilde)
-    out[1] = x[2]
-    out[2] = βu(t, x[2], P)
-    out
-end
-
-function Bridge.σ!(t, x, dm, out, P::PorPtilde)
-    out[1] = 0.0
-    out[2] = dm*P.γ
-    out
-end
-function Bridge.a(t, P::PorPtilde)
-    [0.0 0.0; 0.0 P.γ^2]
-end
-Bridge.a(t, x, P::PorPtilde) = Bridge.a(t, P::PorPtilde)
-
-Bridge.constdiff(::PorPtilde) = true
-
-function Bridge.B!(t, arg, out, P::PBIntegratedDiffusionAux)
-    B = [0.0 1.0; 0.0 -1.0]
-    out .= (B*arg)
-    out
-end
-function BBt!(t, arg, out, P::PBIntegratedDiffusionAux)
-    B = [0.0 1.0; 0.0 -1.0]
-    out .= (B*arg + arg*B')
-    out
-end
-
-function Bridge.dP!(t, p, out, P)
-    BBt!(t, p, out, P)
-    out[2,2] -= P.γ^2
-    out
-end
 
 # Generate Data
 Random.seed!(1)
 
-P = PBIntegratedDiffusion(0.7)
-Pt = PBIntegratedDiffusionAux(0.7)
+P = PBIntegratedDiffusion(γ)
+Pt = PBIntegratedDiffusionAux(γ)
 
 W1 = sample(tt, Wiener())
-x0 = [2.0, 1.0]
+
 X1 = solve(EulerMaruyama!(), x0, W1, P)
 
 
-L = [1. 0.]
-Σnoise = fill(0.01, 1, 1)
-v = [2.5]
+
+
 
 # Solve Backward Recursion
 
 N = length(tt)
 νt = [zero(x0) for i in 1:N]
 Σt = [zero(Bridge.outer(x0)) for i in 1:N]
-ϵ = 0.02
 
-Bridge.partialbridgeode!(Bridge.R3!(), tt, L, Σnoise, v, νt, Σt, Pt, ϵ)
+
+Bridge.partialbridgeode!(Bridge.R3!(), tt, L, Σ, v, νt, Σt, Pt, ϵ)
 
 
 j = length(tt)÷2
@@ -87,14 +39,13 @@ A = Bridge.a(NaN, Pt)
 @test norm((νt[j+1] - νt[j])/dt - (Bridge.b(tt[j+1],  νt[j], Pt))) < 0.01
 j = length(tt) - 3
 @test norm((B*Σt[j+1] + Σt[j+1]*B' - Bridge.a(tt[j+1], Pt)) - Bridge.dP!(tt[j+1], Σt[j+1], copy(Σt[j+1]), Pt)) < 0.01
-@test_broken norm((Σt[j+1] - Σt[j])/dt - (B*Σt[j+1] + Σt[j+1]*B' - Bridge.a(tt[j+1], Pt))) < 0.01
+#@test_broken norm((Σt[j+1] - Σt[j])/dt - (B*Σt[j+1] + Σt[j+1]*B' - Bridge.a(tt[j+1], Pt))) < 0.01
 
-Po1 = Bridge.PartialBridge!(tt, P, Pt, L, v, ϵ, Σnoise)
+Po1 = Bridge.PartialBridge!(tt, P, Pt, L, v, ϵ, Σ)
 
 @test Po1.H == inv.(Σt)
 
 
-x0 = [2.0, 1.0]
 Xo1 = deepcopy(X1)
 
 solve!(Bridge.EulerMaruyama!(), Xo1, x0, W1, Po1)
@@ -159,6 +110,6 @@ function mcmc1(x0, tt, Po2)
 end
 
 @time @testset "MCMC1" begin
-    mcmc1(x0, tt, Po2)
+    mcmc1(x0, tt, Po1)
 
 end
