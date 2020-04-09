0b92b29f79316be8e4c23c8f536718ec5fad3c83
Moritz Schauer <moritzschauer@web.de>
2020-04-09 20:00:58 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,6 @@ using Bridge
 
 makedocs(
     modules = [Bridge],
-    format = Documenter.HTML(),
     sitename = "Bridge.jl",
     authors = "Moritz Schauer and contributors",
     pages = Any[ # Compat: `Any` for 0.4 compat
@@ -18,7 +17,4 @@ makedocs(
 # for more information.
 deploydocs(
     repo = "github.com/mschauer/Bridge.jl.git",
-    target = "build",
-    deps = nothing,
-    make = nothing,
 )
